a4c473cdcc555f52a4707719e1e22a81b4f5fd86
verity <46003573+ladyofthelog@users.noreply.github.com>
2020-08-12 19:27:15 -0400

    Update __init__.py

--- src/ao3/__init__.py
+++ src/ao3/__init__.py
@@ -12,22 +12,18 @@ class AO3(object):
         self.user = None
         self.session = requests.Session()
 
+#bypasses AO3 login to avoid plaintext credential entry
+#user can input in their current AO3 session ID
+    def set_session(self, session):
+        self.session = session
+
     def __repr__(self):
         return '%s()' % (type(self).__name__)
 
+
     def work(self, id):
         """Look up a work that's been posted to AO3.
-
         :param id: the work ID.  In the URL to a work, this is the number.
             e.g. the work ID of http://archiveofourown.org/works/1234 is 1234.
         """
         return Work(id=id, sess=self.session)
-
-    def login(self, username, password):
-        """Log in to the archive.
-
-        This allows you to access pages that are only available while
-        logged in.  This doesn't do any checking that the password is correct.
-
-        """
-        self.user = User(username=username, password=password, sess=self.session)
